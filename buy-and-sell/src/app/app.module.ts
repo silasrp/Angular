327c1300b1f68550e8e688579b93d44420a1b08f
Silas Pereira <silasrp@hotmail.com>
2023-06-29 19:55:25 +0100

    end of 1-12

--- buy-and-sell/src/app/app.module.ts
+++ buy-and-sell/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingDataFormComponent } from './listing-data-form/listing-data-form.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +20,13 @@ import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.
     ContactPageComponent,
     MyListingsPageComponent,
     NewListingPageComponent,
-    EditListingPageComponent
+    EditListingPageComponent,
+    ListingDataFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
+++ buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-new-listing-page',
-  templateUrl: './new-listing-page.component.html',
-  styleUrls: ['./new-listing-page.component.css']
+  selector: 'app-listing-data-form',
+  templateUrl: './listing-data-form.component.html',
+  styleUrls: ['./listing-data-form.component.css']
 })
-export class NewListingPageComponent implements OnInit {
+export class ListingDataFormComponent implements OnInit{
   name: string = '';
   description: string = '';
   price: string = '';
